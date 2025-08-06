ab7dd4da0624beb486ad54d14180afee99d66479
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-08-06 19:29:14 +0200

    refactor: simplify vowel counting function

--- python/7kyu/Vowel_Count.py
+++ python/7kyu/Vowel_Count.py
@@ -1,10 +1,13 @@
+# def get_count(sentence):
+#     vowels = {"a", "e", "i", "o", "u"}
+#     count = 0
+#     for char in sentence:
+#         if char in vowels:
+#             count += 1
+#     return count
+
 def get_count(sentence):
-    vowels = {"a", "e", "i", "o", "u"}
-    count = 0
-    for char in sentence:
-        if char in vowels:
-            count += 1
-    return count
+    return sum(char in 'aeiou' for char in sentence)
 
 
 
