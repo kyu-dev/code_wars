c367b60f5da3277e78c897307cd84b6916075d90
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-08-06 19:25:11 +0200

    7kyu

---
+++ python/7kyu/Vowel_Count.py
@@ -0,0 +1,11 @@
+def get_count(sentence):
+    vowels = {"a", "e", "i", "o", "u"}
+    count = 0
+    for char in sentence:
+        if char in vowels:
+            count += 1
+    return count
+
+
+
+    
